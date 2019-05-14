7f54347833e8f5efa0495fd61e3383f38aad4d01
scrappy1987 <jagordon87@msn.com>
2019-05-14 11:04:55 +0100

    Second commit

--- src/test/java/javacalc/CalculatorTest.java
+++ src/test/java/javacalc/CalculatorTest.java
@@ -8,11 +8,20 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void test() {
+	public void addTest() {
 		Calculator calc = new Calculator();
 		int actualValue = calc.addTwoNumbers(2,2);
 		System.out.println("the value of actual is " + actualValue);
 		Assert.assertEquals(4, actualValue);
 	}
+	
+	@Test
+	public void subtractTest() {
+		Calculator calc = new Calculator();
+		int actualValue = calc.subtract(6,2);
+		System.out.println("the value of actual is " + actualValue);
+		Assert.assertEquals(4, actualValue);
+	}
+
 
 }
